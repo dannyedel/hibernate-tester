6686d158797338f1ec87e1cc9b38633f08ba59a3
Danny Edel <mail@danny-edel.de>
2015-02-16 16:46:21 +0100

    Improved client script, supports IP on cmdline

--- client.sh
+++ client.sh
@@ -5,19 +5,30 @@
 set -e
 set -v
 
-SERVER=10.54.5.129
-PORT=6666
+SERVER="$1"
+PORT="$2"
+
+if [ -z "$SERVER" ] ; then
+	SERVER="10.54.5.129"
+fi
+if [ -z "$PORT" ] ; then
+	PORT="6666"
+fi
 
 while true; do
-	LOADWHAT=$(echo "LOADWHAT" | netcat -vv -q1 $SERVER $PORT)
+	LOADWHAT=$(echo "LOADWHAT" | netcat -vvn -q1 $SERVER $PORT)
 	if [ -z "$LOADWHAT" ] ; then
 		echo "FINISHED"
 		exit 0
 	fi
+	echo "modprobe'ing $LOADWHAT"
 	modprobe -v $LOADWHAT
-	( echo "LOADED" ; cat /proc/modules ) | netcat -vv -q1 $SERVER $PORT
+	( echo "LOADED" ; cat /proc/modules ) | netcat -vvn -q1 $SERVER $PORT
+	echo "REBOOTING"
 	echo reboot > /sys/power/disk
 	echo disk > /sys/power/state
-	sleep 10 # wait for network to recover
-	( echo "KNOWNGOOD" ; cat /proc/modules ) | netcat -vv -q1 $SERVER $PORT
+	echo "Sleeping a bit for the network to get back up"
+	sleep 1 # wait for network to recover
+	echo "Sending known good modules"
+	( echo "KNOWNGOOD" ; cat /proc/modules ) | netcat -vvn -q1 $SERVER $PORT
 done
